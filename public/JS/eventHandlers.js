571fdad2048f497cf48d2264c45575ded5678636
luksab <luksablp@gmail.com>
2020-01-11 11:47:33 +0100

    add onload to eventListeners

--- public/JS/eventHandlers.js
+++ public/JS/eventHandlers.js
@@ -62,14 +62,14 @@ detectswipe('mySidenav', (el, d) => (d === 'l') ? cycleNavBar() : false);
 
 var rightClickEvent = 0;
 
-function addFriendEvent(event){
-  if(event.target.innerHTML === "Yes"){
+function addFriendEvent(event) {
+  if (event.target.innerHTML === "Yes") {
     addFriend(rightClickEvent.target.innerText);
   }
 }
 
-function deleteMsgEvent(event){
-  if(event.target.innerHTML === "Yes" && rightClickEvent.rootEl.parentElement.getAttribute("id") === "lines" && rightClickEvent.rootEl.getAttribute("dbid") !== 0){
+function deleteMsgEvent(event) {
+  if (event.target.innerHTML === "Yes" && rightClickEvent.rootEl.parentElement.getAttribute("id") === "lines" && rightClickEvent.rootEl.getAttribute("dbid") !== 0) {
     var trans = db.transaction(["messages"], 'readwrite');
     var objectStore = trans.objectStore("messages");
     var index = objectStore.index("chat");
@@ -77,49 +77,49 @@ function deleteMsgEvent(event){
     // Make a request to delete the specified record out of the object store
     console.log(rightClickEvent.rootEl.getAttribute("dbid"))
     let id = parseInt(rightClickEvent.rootEl.getAttribute("dbid"));
-    if(id > 0){
+    if (id > 0) {
       var objectStoreRequest = objectStore.delete(id);
-      objectStoreRequest.onsuccess = function(event) {
+      objectStoreRequest.onsuccess = function (event) {
         console.log("deleted successfully");
       };
-      objectStoreRequest.onerror = function(event) {
-        console.log("couldn't delete: "+event);
+      objectStoreRequest.onerror = function (event) {
+        console.log("couldn't delete: " + event);
       };
       rightClickEvent.rootEl.parentElement.removeChild(rightClickEvent.rootEl);
     }
-    else{
+    else {
       message('System', 'Couldn\'t delete message. Reloading might help.');
     }
   }
 }
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
   document.getElementById("rmenu").className = "hide";
-},false);
+}, false);
 if (document.addEventListener) {
-  document.addEventListener('contextmenu', function(e) {
+  document.addEventListener('contextmenu', function (e) {
     rightClickEvent = e;
     console.log(e.target.parentElement)
     //delete Message code
-    let rootEl = e.target.getAttribute("dbid")?e.target:e.target.parentElement;
-    rootEl = rootEl.getAttribute("dbid")?rootEl:null;
+    let rootEl = e.target.getAttribute("dbid") ? e.target : e.target.parentElement;
+    rootEl = rootEl.getAttribute("dbid") ? rootEl : null;
     console.log(e.target.getAttribute("dbid"))
     console.log(rootEl || "Didn't click on a message!")
-    if(rootEl && rootEl.parentElement.getAttribute("id") === "lines" && rootEl.getAttribute("dbid") !== 0){
+    if (rootEl && rootEl.parentElement.getAttribute("id") === "lines" && rootEl.getAttribute("dbid") !== 0) {
       e.preventDefault();
-      document.getElementById("rmenu").innerHTML='<ul><li>Do you really want to delete the message?</li><li id="messageToDelete"> </li><li> <div><a class="danger" style="cursor: pointer;">Yes</a></div><div><a style="cursor: pointer;">No</a></div></li></ul>';
+      document.getElementById("rmenu").innerHTML = '<ul><li>Do you really want to delete the message?</li><li id="messageToDelete"> </li><li> <div><a class="danger" style="cursor: pointer;">Yes</a></div><div><a style="cursor: pointer;">No</a></div></li></ul>';
       document.getElementById("rmenu").className = "show";
       document.getElementById("rmenu").style.top = mouseY(event) + 'px';
       document.getElementById("rmenu").style.left = mouseX(event) + 'px';
       document.getElementById("rmenu").onclick = deleteMsgEvent;
-      if(rootEl.children[1] && rootEl.children[1].tagName === "IMG")
-        document.getElementById("messageToDelete").innerHTML = '<b>'+rootEl.children[0].innerHTML+"</b>: Image";
+      if (rootEl.children[1] && rootEl.children[1].tagName === "IMG")
+        document.getElementById("messageToDelete").innerHTML = '<b>' + rootEl.children[0].innerHTML + "</b>: Image";
       else
         document.getElementById("messageToDelete").innerHTML = rootEl.innerHTML;
       rightClickEvent.rootEl = rootEl;
     }
-    else if(event.target.parentElement.getAttribute("id") === "nicknames" && event.target.tagName === "B"){
+    else if (event.target.parentElement.getAttribute("id") === "nicknames" && event.target.tagName === "B") {
       e.preventDefault();
-      document.getElementById("rmenu").innerHTML='<ul><li>Do you want to add '+event.target.innerText+' as a friend?</li><li id="messageToDelete"> </li><li> <div><a style="cursor: pointer;">Yes</a></div><div><a style="cursor: pointer;">No</a></div></li></ul>';
+      document.getElementById("rmenu").innerHTML = '<ul><li>Do you want to add ' + event.target.innerText + ' as a friend?</li><li id="messageToDelete"> </li><li> <div><a style="cursor: pointer;">Yes</a></div><div><a style="cursor: pointer;">No</a></div></li></ul>';
       document.getElementById("rmenu").className = "show";
       document.getElementById("rmenu").style.top = mouseY(event) + 'px';
       document.getElementById("rmenu").style.left = mouseX(event) + 'px';
@@ -128,7 +128,7 @@ if (document.addEventListener) {
     }
   }, false);
 } else {
-  document.attachEvent('oncontextmenu', function() {
+  document.attachEvent('oncontextmenu', function () {
     window.event.returnValue = false;
   });
 }
@@ -156,142 +156,144 @@ function mouseY(evt) {
 }
 
 
-contactsSearch.onchange = contactsSearch.onkeyup = contactsSearch.onclick = () => {
-  refreshNicks();
-  if (contactsSearch.value != "") {
-    console.log(nicknames);
-    return ws.send(JSON.stringify({ "type": "userSearch", "search": contactsSearch.value }));
-  }
-};
+window.addEventListener('load', (ev)=> {
+  contactsSearch.onchange = contactsSearch.onkeyup = contactsSearch.onclick = () => {
+    refreshNicks();
+    if (contactsSearch.value != "") {
+      console.log(nicknames);
+      return ws.send(JSON.stringify({ "type": "userSearch", "search": contactsSearch.value }));
+    }
+  };
 
-document.getElementById('nicknames').addEventListener('click', (ev) => {
-  const clickedEl = ev.target;
-  if (clickedEl.tagName == 'B') {
-    const nickName = clickedEl.innerText;
-    if (clickedEl.innerText === localStorage.getItem('name')) {
-      const umenu = document.getElementById("umenu");
-      umenu.style.transition = "top 0.3s ease-in-out";
-      umenu.style.top = '2.5vh';
-      setTimeout(() => umenu.style.transition = "top 0s ease-in-out", 500);
-      umenu.innerHTML = '<p>' + nickName + '</p><p>' + localStorage.getItem('signKeyP') + '</p>' +
-        '<p>' + localStorage.getItem('encryptKeyP') + '</p>';
-      umenu.innerHTML += `<form action="#" class="wrap" id="change-nickname">
+  document.getElementById('nicknames').addEventListener('click', (ev) => {
+    const clickedEl = ev.target;
+    if (clickedEl.tagName == 'B') {
+      const nickName = clickedEl.innerText;
+      if (clickedEl.innerText === localStorage.getItem('name')) {
+        const umenu = document.getElementById("umenu");
+        umenu.style.transition = "top 0.3s ease-in-out";
+        umenu.style.top = '2.5vh';
+        setTimeout(() => umenu.style.transition = "top 0s ease-in-out", 500);
+        umenu.innerHTML = '<p>' + nickName + '</p><p>' + localStorage.getItem('signKeyP') + '</p>' +
+          '<p>' + localStorage.getItem('encryptKeyP') + '</p>';
+        umenu.innerHTML += `<form action="#" class="wrap" id="change-nickname">
       <label for="changeNickname" style="color: #00b5d6;">Please type in your nickname and press enter.</label>
       <input id="changeNickname" aria-label="`+ nickName + `" type="text" placeholder="` + nickName + `" autocomplete="off" onclick=""/>
       <input type="submit" id="changeNick" value="change" margin = "10px">
       </form>`
-      document.getElementById("changeNick").onclick = async function () {
-        window.ws.send(JSON.stringify({ "type": "changeName", "name": document.getElementById("changeNickname").value }))
+        document.getElementById("changeNick").onclick = async function () {
+          window.ws.send(JSON.stringify({ "type": "changeName", "name": document.getElementById("changeNickname").value }))
+          const umenu = document.getElementById("umenu");
+          umenu.style.transition = "top 0.3s ease-in-out";
+          umenu.style.top = '-200vh';
+          setTimeout(() => umenu.style.transition = "top 0s ease-in-out", 500);
+        }
+        umenu.onclick = (e) => {
+          if (e.target !== document.getElementById("umenu"))
+            return false;
+          const umenu = document.getElementById("umenu");
+          umenu.style.transition = "top 0.3s ease-in-out";
+          umenu.style.top = '-200vh';
+          setTimeout(() => umenu.style.transition = "top 0s ease-in-out", 500);
+        };
+        return false;
+      } else if (dmName == nickName && nickName !== "allChat") {
         const umenu = document.getElementById("umenu");
         umenu.style.transition = "top 0.3s ease-in-out";
-        umenu.style.top = '-200vh';
+        umenu.style.top = '2.5vh';
         setTimeout(() => umenu.style.transition = "top 0s ease-in-out", 500);
+        umenu.onclick = () => {
+          const umenu = document.getElementById("umenu");
+          umenu.style.transition = "top 0.3s ease-in-out";
+          umenu.style.top = '-200vh';
+          setTimeout(() => umenu.style.transition = "top 0s ease-in-out", 500);
+        };
+        umenu.innerHTML = '<p>' + nickName + '</p><p>' + publicKeys[nickName].signS + '</p><p>' + publicKeys[nickName].encryptS + '</p>';
+        return false;
       }
-      umenu.onclick = (e) => {
-        if (e.target !== document.getElementById("umenu"))
-          return false;
-        const umenu = document.getElementById("umenu");
-        umenu.style.transition = "top 0.3s ease-in-out";
-        umenu.style.top = '-200vh';
-        setTimeout(() => umenu.style.transition = "top 0s ease-in-out", 500);
-      };
-      return false;
-    } else if (dmName == nickName && nickName !== "allChat") {
-      const umenu = document.getElementById("umenu");
-      umenu.style.transition = "top 0.3s ease-in-out";
-      umenu.style.top = '2.5vh';
-      setTimeout(() => umenu.style.transition = "top 0s ease-in-out", 500);
-      umenu.onclick = () => {
-        const umenu = document.getElementById("umenu");
-        umenu.style.transition = "top 0.3s ease-in-out";
-        umenu.style.top = '-200vh';
-        setTimeout(() => umenu.style.transition = "top 0s ease-in-out", 500);
-      };
-      umenu.innerHTML = '<p>' + nickName + '</p><p>' + publicKeys[nickName].signS + '</p><p>' + publicKeys[nickName].encryptS + '</p>';
-      return false;
+      cycleNavBar()
+      dmName = nickName;
+      if (dmName !== "allChat")
+        window.ws.send(JSON.stringify({ "type": "getKey", "uid": dmName }));
+      document.getElementsByTagName('span')[1].innerText = "☰ " + ((dmName === "allChat") ? "Cool Chat" : dmName);
+      lines.innerHTML = '';
+      if (!MessageFromDB(dmName) && dms[dmName] != null)
+        for (let el of dms[dmName])
+          lines.innerHTML += '<p><b>' + el.from + '</b>' + el.msg + '</p>';
+      setTimeout(() => lines.scrollTop = lines.scrollHeight, 10);
     }
-    cycleNavBar()
-    dmName = nickName;
-    if (dmName !== "allChat")
-      window.ws.send(JSON.stringify({ "type": "getKey", "uid": dmName }));
-    document.getElementsByTagName('span')[1].innerText = "☰ " + ((dmName === "allChat") ? "Cool Chat" : dmName);
-    lines.innerHTML = '';
-    if (!MessageFromDB(dmName) && dms[dmName] != null)
-      for (let el of dms[dmName])
-        lines.innerHTML += '<p><b>' + el.from + '</b>' + el.msg + '</p>';
-    setTimeout(() => lines.scrollTop = lines.scrollHeight, 10);
-  }
-});
+  });
 
-document.getElementById('register').onclick = async function () {
-  document.getElementById('register').disabled = true;
-  const passwd = document.getElementById("passwd").value;
-  if (localStorage.getItem("encryptKeyS").indexOf("-----BEGIN RSA PRIVATE KEY-----") !== -1 &&
-    localStorage.getItem("signKeyS").indexOf("-----BEGIN RSA PRIVATE KEY-----") !== -1 &&
-    localStorage.getItem("uid") != null) {
-    console.log("login");
-    let privateEKey = await importPrivateDecryptKey(localStorage.getItem("encryptKeyS"));
-    let privateSKey = await importPrivateDecryptKey(localStorage.getItem("signKeyS"));
-    const toSend = {
-      "type": "login",
-      "uid": parseInt(localStorage.getItem("uid"), 10).toString(32),
-    }
-    console.log(toSend);
-    return window.ws.send(JSON.stringify(toSend));
-  }
-  console.log("register");
-
-  generateKeys(passwd, (enc, sign) => {
-    deriveKey(saltBuf, passwd).then(function (keyBuf) {
-      console.log("keyBuf:", keyBuf);
-      //enc = await encryptMessage(keyBuf,enc);
-      //sign = await encryptMessage(keyBuf,sign);
-      localStorage.setItem("encryptKeyS", enc);
-      localStorage.setItem("signKeyS", sign);
-
-      //console.log(await decryptMessage(keyBuf,enc));//localStorage.getItem('encryptKeyS')));
-
-      enc = localStorage.getItem('encryptKeyP');
-      sign = localStorage.getItem('signKeyP');
+  document.getElementById('register').onclick = async function () {
+    document.getElementById('register').disabled = true;
+    const passwd = document.getElementById("passwd").value;
+    if (localStorage.getItem("encryptKeyS").indexOf("-----BEGIN RSA PRIVATE KEY-----") !== -1 &&
+      localStorage.getItem("signKeyS").indexOf("-----BEGIN RSA PRIVATE KEY-----") !== -1 &&
+      localStorage.getItem("uid") != null) {
+      console.log("login");
+      let privateEKey = await importPrivateDecryptKey(localStorage.getItem("encryptKeyS"));
+      let privateSKey = await importPrivateDecryptKey(localStorage.getItem("signKeyS"));
       const toSend = {
-        "type": "register",
-        "keys": { "enc": enc, "sign": sign },
+        "type": "login",
+        "uid": parseInt(localStorage.getItem("uid"), 10).toString(32),
       }
-      window.ws.send(JSON.stringify(toSend));
-    });
-  });
-  return false;
-}
-
-document.getElementById('send-message').addEventListener(
-  "submit", function (e) {
-    e.preventDefault();
-    let uuid = generateUUID();
-    const messageElement = document.getElementById("message");
-    if (dmName) {
-      if (publicKeys[dmName])
-        encryptData(publicKeys[dmName].encrypt, messageElement.value).then((encyptedData) => {
-          signData(signKey, encyptedData).then((SignedTest) => {
-            dm('me', messageElement.value, uuid, true);
-            console.log("sending Encrypted MSG");
-            console.log(JSON.stringify({ "type": "encryptedDM", "user": dmName, "msg": encyptedData, "signature": SignedTest, "uuid": uuid }))
-            window.ws.send(JSON.stringify({ "type": "encryptedDM", "user": dmName, "msg": encyptedData, "signature": SignedTest, "uuid": uuid }));
-            messageElement.value = "";
-            messageElement.focus();
-            document.getElementById("lines").scrollTop = lines.scrollHeight;
-            //$('#lines').get(0).scrollTop = lines.scrollHeight;
-          })
-        })
-      else {
-        dm('me', messageElement.value, uuid);
-        console.log(JSON.stringify({ "type": "dm", "user": dmName, "msg": messageElement.value, "uuid": uuid }))
-        window.ws.send(JSON.stringify({ "type": "dm", "user": dmName, "msg": messageElement.value, "uuid": uuid }));
-        messageElement.value = "";
-        messageElement.focus();
-        document.getElementById("lines").scrollTop = lines.scrollHeight;
-        //$('#lines').get(0).scrollTop = lines.scrollHeight;
-      }
-      return false;
+      console.log(toSend);
+      return window.ws.send(JSON.stringify(toSend));
     }
-    alert("No dm");
-  });
+    console.log("register");
+
+    generateKeys(passwd, (enc, sign) => {
+      deriveKey(saltBuf, passwd).then(function (keyBuf) {
+        console.log("keyBuf:", keyBuf);
+        //enc = await encryptMessage(keyBuf,enc);
+        //sign = await encryptMessage(keyBuf,sign);
+        localStorage.setItem("encryptKeyS", enc);
+        localStorage.setItem("signKeyS", sign);
+
+        //console.log(await decryptMessage(keyBuf,enc));//localStorage.getItem('encryptKeyS')));
+
+        enc = localStorage.getItem('encryptKeyP');
+        sign = localStorage.getItem('signKeyP');
+        const toSend = {
+          "type": "register",
+          "keys": { "enc": enc, "sign": sign },
+        }
+        window.ws.send(JSON.stringify(toSend));
+      });
+    });
+    return false;
+  }
+
+  document.getElementById('send-message').addEventListener(
+    "submit", function (e) {
+      e.preventDefault();
+      let uuid = generateUUID();
+      const messageElement = document.getElementById("message");
+      if (dmName) {
+        if (publicKeys[dmName])
+          encryptData(publicKeys[dmName].encrypt, messageElement.value).then((encyptedData) => {
+            signData(signKey, encyptedData).then((SignedTest) => {
+              dm('me', messageElement.value, uuid, true);
+              console.log("sending Encrypted MSG");
+              console.log(JSON.stringify({ "type": "encryptedDM", "user": dmName, "msg": encyptedData, "signature": SignedTest, "uuid": uuid }))
+              window.ws.send(JSON.stringify({ "type": "encryptedDM", "user": dmName, "msg": encyptedData, "signature": SignedTest, "uuid": uuid }));
+              messageElement.value = "";
+              messageElement.focus();
+              document.getElementById("lines").scrollTop = lines.scrollHeight;
+              //$('#lines').get(0).scrollTop = lines.scrollHeight;
+            })
+          })
+        else {
+          dm('me', messageElement.value, uuid);
+          console.log(JSON.stringify({ "type": "dm", "user": dmName, "msg": messageElement.value, "uuid": uuid }))
+          window.ws.send(JSON.stringify({ "type": "dm", "user": dmName, "msg": messageElement.value, "uuid": uuid }));
+          messageElement.value = "";
+          messageElement.focus();
+          document.getElementById("lines").scrollTop = lines.scrollHeight;
+          //$('#lines').get(0).scrollTop = lines.scrollHeight;
+        }
+        return false;
+      }
+      alert("No dm");
+    });
+});
